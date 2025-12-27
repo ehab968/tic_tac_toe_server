b9a1f9f5fb5744ea72fd95e87987f0181c931385
ELDemy <mahmoudeldemerdash5@gmail.com>
2025-12-27 19:53:08 +0200

    get the server app ready for the installation file

---
+++ TicTacToeServer/src/main/java/com/mycompany/tictactoeserver/Launcher.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tictactoeserver;
+
+/**
+ *
+ * @author mahmo
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        App.main(args); // This calls your actual JavaFX App class
+    }
+}
